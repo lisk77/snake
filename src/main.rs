c92db05d5fcda4e41109da307812f9a610b14f97
lisk77 <lisk77@protonmail.com>
2026-02-26 23:22:17 +0100

    feat: basic bundles and components as well as textures

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
 use comet::prelude::*;
 
-fn setup(app: &mut App, renderer: &mut RenderHandle2D) {}
+#[derive(Component)]
+struct Snake;
 
-fn update(app: &mut App, renderer: &mut RenderHandle2D, dt: f32) {}
+#[derive(Component)]
+struct Direction {
+    x: i8,
+    y: i8,
+}
+
+bundle!(Camera {
+    transform: Transform2D,
+    camera: Camera2D
+});
+
+bundle!(SnakeSegment {
+    snake: Snake,
+    dir: Direction,
+    transform: Transform2D,
+    render: Render2D
+});
+
+fn setup(app: &mut App, renderer: &mut RenderHandle2D) {
+    renderer.init_atlas();
+
+    app.register_component::<Snake>();
+    app.register_component::<Direction>();
+
+    app.spawn_bundle(Camera {
+        transform: Transform2D::new(),
+        camera: Camera2D::new(v2::new(1.0, 1.0), 1.0, 1),
+    });
+
+    app.spawn_bundle(SnakeSegment {
+        snake: Snake,
+        dir: Direction::new(),
+        transform: Transform2D::new(),
+        render: Render2D::with_texture("res/textures/snake_body.png"),
+    });
+}
+
+fn update(app: &mut App, renderer: &mut RenderHandle2D, dt: f32) {
+    renderer.render_scene_2d(app.scene_mut());
+}
 
 fn main() {
     App::new()
