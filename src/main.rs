5b4623cb423f2c889f114e9ce7ed362093ffc0e4
lisk77 <lisk77@protonmail.com>
2026-03-13 23:55:21 +0100

    refactor: applied new query api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use comet::prelude::*;
 
-#[derive(Tag)]
+#[derive(Component)]
 struct Snake;
 
-#[derive(Tag)]
+#[derive(Component)]
 struct Apple;
 
-#[derive(Tag)]
+#[derive(Component)]
 struct GameOverText;
 
-#[derive(Tag)]
+#[derive(Component)]
 struct WinText;
 
 #[derive(Component)]
@@ -63,12 +63,15 @@ impl Grid {
 
 struct GameState {
     game_over: bool,
-    win: bool
+    win: bool,
 }
 
 impl GameState {
     pub fn new() -> Self {
-        Self { game_over: false, win: false }
+        Self {
+            game_over: false,
+            win: false,
+        }
     }
 
     pub fn is_game_over(&self) -> bool {
@@ -188,7 +191,7 @@ fn setup(app: &mut App, renderer: &mut RenderHandle2D) {
         collider: Rectangle2D::with_size(grid_size, grid_size),
         render: Render2D::with_texture("res/textures/field.png"),
     });
-    
+
     app.spawn_bundle(AppleEntity {
         apple: Apple,
         transform: Transform2D::new(),
@@ -196,22 +199,38 @@ fn setup(app: &mut App, renderer: &mut RenderHandle2D) {
         render: Render2D::new("res/textures/apple.png", true, v2::new(1.0, 1.0), 1),
     });
 
-    let game_over_text_bounds = renderer.precompute_text_bounds("Game Over!", "res/fonts/PressStart2P-Regular.ttf", 16.0);
-    let win_text_bounds = renderer.precompute_text_bounds("You Win!", "res/fonts/PressStart2P-Regular.ttf", 16.0);
+    let game_over_text_bounds =
+        renderer.precompute_text_bounds("Game Over!", "res/fonts/PressStart2P-Regular.ttf", 16.0);
+    let win_text_bounds =
+        renderer.precompute_text_bounds("You Win!", "res/fonts/PressStart2P-Regular.ttf", 16.0);
 
-    let game_over_text_transform = Transform2D::with_position(Position2D::from_vec(-game_over_text_bounds/2.0));
-    let win_text_transform = Transform2D::with_position(Position2D::from_vec(-win_text_bounds/2.0));
+    let game_over_text_transform =
+        Transform2D::with_position(Position2D::from_vec(-game_over_text_bounds / 2.0));
+    let win_text_transform =
+        Transform2D::with_position(Position2D::from_vec(-win_text_bounds / 2.0));
 
     let game_over_text = app.spawn_bundle(GameText {
         transform: game_over_text_transform,
-        render: Text::new("Game Over!", "res/fonts/PressStart2P-Regular.ttf", 16.0, false, sRgba::<f32>::from_hex("#ff0000ff")),
+        render: Text::new(
+            "Game Over!",
+            "res/fonts/PressStart2P-Regular.ttf",
+            16.0,
+            false,
+            sRgba::<f32>::from_hex("#ff0000ff"),
+        ),
     });
 
     app.add_component(game_over_text, GameOverText);
 
     let win_text = app.spawn_bundle(GameText {
         transform: win_text_transform,
-        render: Text::new("You Win!", "res/fonts/PressStart2P-Regular.ttf", 16.0, false, sRgba::<f32>::from_hex("#0000ffff")),
+        render: Text::new(
+            "You Win!",
+            "res/fonts/PressStart2P-Regular.ttf",
+            16.0,
+            false,
+            sRgba::<f32>::from_hex("#0000ffff"),
+        ),
     });
 
     app.add_component(win_text, WinText);
@@ -222,8 +241,7 @@ fn setup(app: &mut App, renderer: &mut RenderHandle2D) {
 fn update(app: &mut App, renderer: &mut RenderHandle2D, _dt: f32) {
     if !app.game_state::<GameState>().unwrap().has_won() {
         let head_pos = app
-            .query::<Transform2D>()
-            .with::<Snake>()
+            .query::<&Transform2D, With<Snake>>()
             .iter()
             .next()
             .unwrap()
@@ -234,37 +252,40 @@ fn update(app: &mut App, renderer: &mut RenderHandle2D, _dt: f32) {
         if !snake_out_of_bounds(app) && !is_snake_body_colliding(app) {
             handle_input(app, head_pos);
             update_snake(app);
-        }
-        else {
+        } else {
             if !app.game_state::<GameState>().unwrap().is_game_over() {
                 app.play_audio("bonk", false);
                 app.set_volume("bonk", 0.5);
-                app.game_state_mut::<GameState>().unwrap().set_game_over(true);
-                app.query_mut::<Text>().with::<GameOverText>().for_each(|t| t.set_visibility(true));
+                app.game_state_mut::<GameState>()
+                    .unwrap()
+                    .set_game_over(true);
+                app.query::<&mut Text, With<GameOverText>>()
+                    .for_each(|t| t.set_visibility(true));
             }
         }
         if winning_condition_met(app) {
             debug!("hello there");
             app.game_state_mut::<GameState>().unwrap().set_win(true);
-            app.query_mut::<Text>().with::<WinText>().for_each(|t| t.set_visibility(true));
+            app.query::<&mut Text, With<WinText>>()
+                .for_each(|t| t.set_visibility(true));
         }
     }
     renderer.render_scene_2d(app.scene_mut());
 }
 
 fn update_timers(app: &mut App, dt: f32) {
-    app.query_mut::<Timer>().for_each(|t| {
+    app.query::<&mut Timer, ()>().for_each(|t| {
         t.update_timer(dt);
     });
 }
 
 fn resize_game_camera(app: &mut App, renderer: &mut RenderHandle2D) {
-    let (grid_cells, grid_cell_size) = match app.query::<Grid>().iter().next() {
+    let (grid_cells, grid_cell_size) = match app.query::<&Grid, ()>().iter().next() {
         Some(g) => (g.cells(), g.cell_size()),
         None => return,
     };
 
-    app.query_mut::<Camera2D>().for_each(|c| {
+    app.query::<&mut Camera2D, ()>().for_each(|c| {
         let size = renderer.size();
         let scale_factor = renderer.scale_factor() as f32;
 
@@ -293,7 +314,7 @@ fn resize_game_camera(app: &mut App, renderer: &mut RenderHandle2D) {
 
 fn update_snake(app: &mut App) {
     let is_done = app
-        .query::<Timer>()
+        .query::<&Timer, ()>()
         .iter()
         .next()
         .map(|t| t.is_done())
@@ -305,7 +326,7 @@ fn update_snake(app: &mut App) {
 
     update_snake_direction(app);
     update_snake_colliders(app);
-    
+
     if !snake_out_of_bounds(app) && !is_snake_body_colliding(app) {
         update_snake_orientation(app);
         handle_apple_collision(app);
@@ -313,43 +334,47 @@ fn update_snake(app: &mut App) {
         update_snake_textures(app);
     }
 
-    app.query_mut::<Timer>().iter().next().unwrap().reset();
+    app.query::<&mut Timer, ()>().iter().next().unwrap().reset();
 }
 
 fn update_snake_direction(app: &mut App) {
     let mut directions = app
-        .query::<Direction>()
+        .query::<&Direction, ()>()
         .iter()
         .map(|d| d.direction())
         .collect::<Vec<v2>>();
     directions.pop();
 
-    let head_dir = app.query_mut::<Direction>().iter().next().unwrap();
+    let head_dir = app.query::<&mut Direction, ()>().iter().next().unwrap();
     head_dir.update();
 
-    for (i, d) in app.query_mut::<Direction>().iter().skip(1).enumerate() {
+    for (i, d) in app.query::<&mut Direction, ()>().iter().skip(1).enumerate() {
         d.set_direction(directions[i]);
     }
 }
 
 fn update_snake_orientation(app: &mut App) {
-    app.query_mut::<(Transform2D, Direction)>()
-        .for_each(|t, c| {
-            let dir = c.direction();
+    app.query::<(&mut Transform2D, &Direction), ()>()
+        .for_each(|(t, d)| {
+            let dir = d.direction();
             let angle = dir.y().atan2(dir.x());
             t.set_rotation((angle.to_degrees() - 90.0).to_radians());
         });
 }
 
 fn update_snake_textures(app: &mut App) {
-    let snake_size = app.query::<Snake>().iter().count();
+    let snake_size = app.query::<&Snake, ()>().iter().count();
     let directions = app
-        .query::<Direction>()
+        .query::<&Direction, ()>()
         .iter()
         .map(|d| d.direction())
         .collect::<Vec<v2>>();
-    
-    for (i, (r, t)) in app.query_mut::<(Render2D, Transform2D)>().with::<Snake>().iter().enumerate() {
+
+    for (i, (r, t)) in app
+        .query::<(&mut Render2D, &mut Transform2D), With<Snake>>()
+        .iter()
+        .enumerate()
+    {
         if i == 0 {
             r.set_texture("res/textures/snake_head.png");
             continue;
@@ -357,13 +382,16 @@ fn update_snake_textures(app: &mut App) {
 
         if i == (snake_size - 1) {
             r.set_texture("res/textures/snake_tail.png");
-            t.set_rotation(directions[i-1].y().atan2(directions[i-1].x()) - std::f32::consts::FRAC_PI_2);
+            t.set_rotation(
+                directions[i - 1].y().atan2(directions[i - 1].x()) - std::f32::consts::FRAC_PI_2,
+            );
             continue;
         }
 
         r.set_texture("res/textures/snake_body.png");
 
-        let det = directions[i].x() * directions[i-1].y() - directions[i].y() * directions[i-1].x();
+        let det =
+            directions[i].x() * directions[i - 1].y() - directions[i].y() * directions[i - 1].x();
         if det > 0.0 {
             r.set_texture("res/textures/snake_turn_left.png");
         } else if det == 0.0 {
@@ -375,17 +403,15 @@ fn update_snake_textures(app: &mut App) {
 }
 
 fn update_snake_position(app: &mut App) {
-    let cell_size = app.query::<Grid>().iter().next().unwrap().cell_size();
+    let cell_size = app.query::<&Grid, ()>().iter().next().unwrap().cell_size();
     let positions = app
-        .query::<Transform2D>()
-        .with::<Snake>()
+        .query::<&Transform2D, With<Snake>>()
         .iter()
         .map(|t| t.position().as_vec())
         .collect::<Vec<v2>>();
 
     for (i, (t, d)) in app
-        .query_mut::<(Transform2D, Direction)>()
-        .with::<Snake>()
+        .query::<(&mut Transform2D, &Direction), With<Snake>>()
         .iter()
         .enumerate()
     {
@@ -394,17 +420,15 @@ fn update_snake_position(app: &mut App) {
 }
 
 fn update_snake_colliders(app: &mut App) {
-    let cell_size = app.query::<Grid>().iter().next().unwrap().cell_size();
+    let cell_size = app.query::<&Grid, ()>().iter().next().unwrap().cell_size();
     let positions = app
-        .query::<Transform2D>()
-        .with::<Snake>()
+        .query::<&Transform2D, With<Snake>>()
         .iter()
         .map(|t| t.position().as_vec())
         .collect::<Vec<v2>>();
 
     for (i, (r, d)) in app
-        .query_mut::<(Rectangle2D, Direction)>()
-        .with::<Snake>()
+        .query::<(&mut Rectangle2D, &Direction), With<Snake>>()
         .iter()
         .enumerate()
     {
@@ -413,26 +437,35 @@ fn update_snake_colliders(app: &mut App) {
 }
 
 fn snake_out_of_bounds(app: &mut App) -> bool {
-    let field_collider = app.query::<(Rectangle2D, Grid)>().iter().next().unwrap().0;
+    let field_collider = app
+        .query::<(&Rectangle2D, &Grid), ()>()
+        .iter()
+        .next()
+        .unwrap()
+        .0
+        .clone();
 
     let snake_head_collider = app
-        .query::<Rectangle2D>()
-        .with::<Snake>()
+        .query::<&Rectangle2D, With<Snake>>()
         .iter()
         .next()
         .unwrap();
 
-    !snake_head_collider.is_colliding(field_collider)
+    !snake_head_collider.is_colliding(&field_collider)
 }
 
 fn move_apple(app: &mut App) {
-    let cell_size = app.query::<Grid>().iter().next().unwrap().cell_size();
-    let half_cells = (app.query::<Grid>().iter().next().unwrap().cells()/2) as i8;
-    let snake = app.query::<Transform2D>().with::<Snake>().iter().map(|t| t.position().as_vec()).collect::<Vec<v2>>();
+    let cell_size = app.query::<&Grid, ()>().iter().next().unwrap().cell_size();
+    let half_cells = (app.query::<&Grid, ()>().iter().next().unwrap().cells() / 2) as i8;
+    let snake = app
+        .query::<&Transform2D, With<Snake>>()
+        .iter()
+        .map(|t| t.position().as_vec())
+        .collect::<Vec<v2>>();
 
     let x = rand::random_range(-half_cells..half_cells) as f32 * cell_size;
     let y = rand::random_range(-half_cells..half_cells) as f32 * cell_size;
-    
+
     let mut apple_pos = Position2D::from_vec(v2::new(x, y));
     while snake.iter().any(|&s| s == apple_pos.as_vec()) {
         let x = rand::random_range(-half_cells..half_cells) as f32 * cell_size;
@@ -440,58 +473,68 @@ fn move_apple(app: &mut App) {
         apple_pos = Position2D::from_vec(v2::new(x, y));
     }
 
-    app.query_mut::<(Transform2D, Rectangle2D)>()
-        .with::<Apple>()
-        .for_each(|t, r| {
+    app.query::<(&mut Transform2D, &mut Rectangle2D), With<Apple>>()
+        .for_each(|(t, r)| {
             t.set_position(apple_pos.clone());
             r.set_position(apple_pos.clone());
         });
 }
 
 fn handle_apple_collision(app: &mut App) {
-    let grid_cell_size = app.query::<Grid>().iter().next().unwrap().cell_size();
-    let apple_collider = app.query::<Rectangle2D>().with::<Apple>().iter().next();
+    let grid_cell_size = app.query::<&Grid, ()>().iter().next().unwrap().cell_size();
+    let apple_collider = app
+        .query::<&Rectangle2D, With<Apple>>()
+        .iter()
+        .next()
+        .unwrap()
+        .clone();
     let snake_head_collider = app
-        .query::<Rectangle2D>()
-        .with::<Snake>()
+        .query::<&Rectangle2D, With<Snake>>()
         .iter()
         .next()
         .unwrap();
 
-    snake_head_collider
-        .is_colliding(apple_collider.unwrap())
-        .then(|| {
-            app.play_audio("nom", false);
-            app.set_volume("nom", 0.5);
+    snake_head_collider.is_colliding(&apple_collider).then(|| {
+        app.play_audio("nom", false);
+        app.set_volume("nom", 0.5);
 
-            move_apple(app);
+        move_apple(app);
 
-            let (tail_transform, tail_dir) = app.query::<(Transform2D, Direction)>().with::<Snake>().iter().last().unwrap();
-            let tail_pos = tail_transform.position().as_vec();
-            let new_tail_pos = tail_pos - tail_dir.direction() * grid_cell_size;
-            let mut new_tail_dir = Direction::new();
-            new_tail_dir.set_direction(tail_dir.direction());
+        let (tail_transform, tail_dir) = app
+            .query::<(&Transform2D, &Direction), With<Snake>>()
+            .iter()
+            .last()
+            .unwrap();
+        let tail_pos = tail_transform.position().as_vec();
+        let new_tail_pos = tail_pos - tail_dir.direction() * grid_cell_size;
+        let mut new_tail_dir = Direction::new();
+        new_tail_dir.set_direction(tail_dir.direction());
 
-            app.spawn_bundle(SnakeSegment {
-                snake: Snake,
-                dir: new_tail_dir,
-                transform: Transform2D::with_position(Position2D::from_vec(new_tail_pos)),
-                collider: Rectangle2D::with_size(16.0, 16.0),
-                render: Render2D::new("res/textures/snake_body.png", true, v2::new(1.0, 1.0), 1),
-            });
+        app.spawn_bundle(SnakeSegment {
+            snake: Snake,
+            dir: new_tail_dir,
+            transform: Transform2D::with_position(Position2D::from_vec(new_tail_pos)),
+            collider: Rectangle2D::with_size(16.0, 16.0),
+            render: Render2D::new("res/textures/snake_body.png", true, v2::new(1.0, 1.0), 1),
+        });
     });
 }
 
 fn is_snake_body_colliding(app: &mut App) -> bool {
-    let head_collider = app.query::<Rectangle2D>().with::<Snake>().iter().next().unwrap();
-    let mut body_colliders = app.query::<Rectangle2D>().with::<Snake>().iter().skip(1);
+    let head_collider = app
+        .query::<&Rectangle2D, With<Snake>>()
+        .iter()
+        .next()
+        .unwrap()
+        .clone();
+    let mut body_colliders = app.query::<&Rectangle2D, With<Snake>>().iter().skip(1);
 
     body_colliders.any(|b| head_collider.is_colliding(b))
 }
 
 fn winning_condition_met(app: &mut App) -> bool {
-    let snake_size = app.query::<Snake>().iter().count();
-    let grid_cells = app.query::<Grid>().iter().next().unwrap().cells() as usize;
+    let snake_size = app.query::<&Snake, ()>().iter().count();
+    let grid_cells = app.query::<&Grid, ()>().iter().next().unwrap().cells() as usize;
 
     snake_size == grid_cells * grid_cells
 }
@@ -513,7 +556,7 @@ fn handle_input(app: &mut App, head_pos: v2) {
         return;
     }
 
-    let head_dir = app.query_mut::<Direction>().iter().next().unwrap();
+    let head_dir = app.query::<&mut Direction, ()>().iter().next().unwrap();
     if new_direction != -head_dir.direction() {
         head_dir.set_buffered_dir(new_direction);
     }
